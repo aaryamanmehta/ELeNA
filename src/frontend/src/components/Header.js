a66f94bd0eb2fab2362a9b124a8dd417177569fc
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-05-01 19:16:22 -0400

    Cleaned up React setup files

---
+++ src/frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+      <div 
+        style = {{
+          fontSize: '32px',
+          color: '#333333',
+          fontWeight: 'bold',
+          textAlign: 'center',
+          fontFamily: 'Helvetica',
+        }}>
+          <p>
+            ELeNA
+          </p>
+      </div>
+    );
+  }
+  
+  export default Header;
--- src/frontend/src/components/Map.js
+++ src/frontend/src/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
+
 function Map() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "100vh" }}>

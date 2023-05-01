8d90f311cf66e8bc7e0286d0b459ca8b21d86902
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-05-01 18:43:02 -0400

    Setup frontend with Leaflet

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Map from './Map.js';
+
+function App() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/frontend/src/Map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+function Map() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "100vh" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
+
+export default Map;

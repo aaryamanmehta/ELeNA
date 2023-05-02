7b386178a82ade66d874683ba6b8075cd5e5d682
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-05-01 20:55:38 -0400

    Initialized sidebar toggle button

--- src/frontend/src/components/Map.js
+++ src/frontend/src/components/Map.js
@@ -11,7 +11,7 @@ function Map() {
     border: "2px #2C3639 solid",
   }
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={style}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={style} zoomControl={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

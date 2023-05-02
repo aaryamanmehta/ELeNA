8560d0bb6458e02fe6d6f564b9fbc52431525344
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-05-02 10:41:54 -0400

    Incremental styling changes to Sidebar

--- src/frontend/src/components/Sidebar.js
+++ src/frontend/src/components/Sidebar.js
@@ -22,24 +22,24 @@ function Sidebar() {
         }}>
           <a style={{display:'flex', marginBottom:'2vh', marginTop:'15vh'}}>Origin</a>
           <form> 
-            <input type="text" name="origin" style={{width: '95%', padding: '4px', marginBottom:'2vh'}}/>
+            <input type="text" name="origin" style={{width: '95%', padding: '4px', marginBottom:'2vh', borderRadius:'7px'}}/>
           </form>
           <br></br>
           <a style={{display:'flex', marginBottom:'2vh'}}>Destination</a>
           <form>
-            <input type="text" name="destination" style={{width: '95%', padding: '4px', marginBottom:'2vh'}}/>
+            <input type="text" name="destination" style={{width: '95%', padding: '4px', marginBottom:'2vh', borderRadius:'7px'}}/>
           </form>
           <br></br>
-          <a style={{display:'flex', marginBottom:'2vh'}}>Elevation Preference</a>
+          <a style={{display:'flex', marginBottom:'0vh'}}>Elevation Preference</a>
           <br></br>
-          <button id="no-elevation">
+          <button id="no-elevation" style={{width: '95%', padding:'4px', marginBottom:'2vh', borderRadius:'7px'}}>
             <a>No Elevation</a>
           </button>
           <br></br>
-          <button id="maximize-elevation">
+          <button id="maximize-elevation" style={{width: '45%', padding:'4px', marginBottom:'2vh', marginRight:'0.5vw', borderRadius:'7px'}}>
             <a>Maximize Elevation</a>
           </button>
-          <button id="minimize-elevation">
+          <button id="minimize-elevation" style={{width: '45%', padding:'4px', marginBottom:'2vh', borderRadius:'7px'}}>
             <a>Minimize Elevation</a>
           </button>
           <br></br>

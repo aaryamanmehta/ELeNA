95de032e088d69023ecc30224b0aeabfd4b39e86
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-05-02 10:53:10 -0400

    Incremental CSS style changes to Sidebar

--- src/frontend/src/components/Sidebar.js
+++ src/frontend/src/components/Sidebar.js
@@ -22,24 +22,24 @@ function Sidebar() {
         }}>
           <a style={{display:'flex', marginBottom:'2vh', marginTop:'15vh'}}>Origin</a>
           <form> 
-            <input type="text" name="origin" style={{width: '95%', padding: '4px', marginBottom:'2vh', borderRadius:'7px'}}/>
+            <input type="text" name="origin" style={{width: '95%', padding: '4px', marginBottom:'2vh', border:'none', borderRadius:'7px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}/>
           </form>
           <br></br>
           <a style={{display:'flex', marginBottom:'2vh'}}>Destination</a>
           <form>
-            <input type="text" name="destination" style={{width: '95%', padding: '4px', marginBottom:'2vh', borderRadius:'7px'}}/>
+            <input type="text" name="destination" style={{width: '95%', padding: '4px', marginBottom:'2vh', border:'none', borderRadius:'7px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}/>
           </form>
           <br></br>
           <a style={{display:'flex', marginBottom:'0vh'}}>Elevation Preference</a>
           <br></br>
-          <button id="no-elevation" style={{width: '95%', padding:'4px', marginBottom:'2vh', borderRadius:'7px'}}>
+          <button id="no-elevation" style={{width: '95%', padding:'4px', marginBottom:'2vh', border:'none', borderRadius:'7px', cursor:'pointer', fontFamily: 'Domine', fontSize: '12px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
             <a>No Elevation</a>
           </button>
           <br></br>
-          <button id="maximize-elevation" style={{width: '45%', padding:'4px', marginBottom:'2vh', marginRight:'0.5vw', borderRadius:'7px'}}>
+          <button id="maximize-elevation" style={{width: '46%', padding:'4px', marginBottom:'2vh', marginRight:'0.5vw', border:'none', borderRadius:'7px', cursor:'pointer', fontFamily: 'Domine', fontSize: '12px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
             <a>Maximize Elevation</a>
           </button>
-          <button id="minimize-elevation" style={{width: '45%', padding:'4px', marginBottom:'2vh', borderRadius:'7px'}}>
+          <button id="minimize-elevation" style={{width: '46%', padding:'4px', marginBottom:'2vh', border:'none', borderRadius:'7px', cursor:'pointer', fontFamily: 'Domine', fontSize: '12px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
             <a>Minimize Elevation</a>
           </button>
           <br></br>

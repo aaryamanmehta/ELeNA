312308115ee07660a750018f160666056573485a
Aryan <aryandang43@gmail.com>
2023-05-12 00:48:31 -0400

    adds docstring providing overview of the function

---
+++ src/backend/DataMod/dijkstra.py
@@ -0,0 +1,12 @@
+'''
+This method returns the shortest path from source to destination
+
+input : 
+source and destination represent the locations as `tuples of coordinates`
+graph is the loaded graph containing both source and destination
+
+return:
+the shortest path from source to destination
+'''
+def dijkstra(source : tuple, destination : tuple, graph):
+    pass

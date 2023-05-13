eca529d4be8d696ae3916766459f686dbd18bd61
Aryan <aryandang43@gmail.com>
2023-05-13 18:29:04 -0400

    adds code to load a saved graph, given the filename

--- src/backend/DataMod/GraphHelper.py
+++ src/backend/DataMod/GraphHelper.py
@@ -3,3 +3,10 @@ import osmnx as ox
 def save_graph(place, filename):
     G = ox.graph_from_place(place, network_type="walk")
     ox.save_graphml(G, filename)
+
+'''
+Returns the saved graph for the given filename
+'''
+def load_graph(filename):
+    # it's guarenteed this file exists
+    return ox.load_graphml(filename)

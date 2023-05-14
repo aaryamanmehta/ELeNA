e8af73880b9132c6e39b44d4d5f92c14763807cf
Aryan <aryandang43@gmail.com>
2023-05-14 00:52:54 -0400

    adds basic code for no-elevation endpoint

--- src/backend/server.py
+++ src/backend/server.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/no-elevation/{route_info}")
+async def shortest_route_no_elevation():
+    return 1 

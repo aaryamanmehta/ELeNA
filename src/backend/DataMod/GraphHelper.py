ce15e05b7725f3bdb682995c0b7b480d1b8024c3
Aryan <aryandang43@gmail.com>
2023-05-20 07:03:37 -0400

    updates the comments so they can show up as Docstrings thus improving Readability

--- src/backend/DataMod/GraphHelper.py
+++ src/backend/DataMod/GraphHelper.py
@@ -4,12 +4,12 @@ def save_graph(place, filename):
     G = ox.graph_from_place(place, network_type="walk")
     ox.save_graphml(G, filename)
 
-'''
-Returns the saved graph for the given filename
-
-input : name of the file where the graph is saved
-return : OSMnx Graph Object
-'''
 def load_graph(filename):
+    '''
+    Returns the saved graph for the given filename
+
+    input : name of the file where the graph is saved
+    return : OSMnx Graph Object
+    '''
     # it's guarenteed this file exists
     return ox.load_graphml(filename)

de52273ef3c193a26c9afec6bb57474057344583
Aryan <aryandang43@gmail.com>
2023-05-20 22:06:22 -0400

    updates code to return a list of latitude and longitude instead of the node ids to render the path more easily on the front-end

--- src/backend/controller/shortestPathNoElevation.py
+++ src/backend/controller/shortestPathNoElevation.py
@@ -5,7 +5,9 @@ def get_shortest_path(source_request : str, destination_request : str):
     source_info = get_location(source_request)
     destination_info = get_location(destination_request)
     graph = generate_graph(source_info, destination_info)
+    # graph = load_graph("DataMod/GeneratedMaps/Amherst.graphml")
     source_coords = get_coordinates(source_request)
     destination_coords = get_coordinates(destination_request)
     path = dijkstra(source_coords, destination_coords, graph)
+    path = [[graph.nodes[node_id]["y"], graph.nodes[node_id]["x"]] for node_id in path]
     return path

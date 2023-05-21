60142a9cf520a4988a33fc907aa912a056796790
AvadRaul20 <72758924+AvadRaul20@users.noreply.github.com>
2023-05-21 19:11:48 -0400

    Removed unecessary info in ElevationDijkstra.py

--- src/backend/DataMod/ElevationDijkstra.py
+++ src/backend/DataMod/ElevationDijkstra.py
@@ -11,17 +11,6 @@ def min_elevation_weight(G, source, dest, weight):
     return weight['length'] + elevation_diff
 
 def findshortestPathElevation(G, source, dest, elevation):
-    # path = []
-    # source_node = ox.distance.nearest_nodes(G, source[0], source[1])
-    # dest_node = ox.distance.nearest_nodes(G, dest[0], dest[1])
-    # for u, v, edge_data in G.edges(data=True):
-    #     edge_data['custom_weight_max'] = max_elevation_weight(G, u, v, edge_data)
-    #     edge_data['custom_weight_min'] = min_elevation_weight(G, u, v, edge_data)
-    # if elevation == "max":
-    #     path = nx.shortest_path(G, source=source_node, target=dest_node, weight='custom_weight_max')
-    # elif elevation == "min":
-    #     path = nx.shortest_path(G, source=source_node, target=dest_node, weight='custom_weight_min')
-    # return path
     source_node = ox.distance.nearest_nodes(G, source[0], source[1])
     dest_node = ox.distance.nearest_nodes(G, dest[0], dest[1])
     for u, v, edge_data in G.edges(data=True):

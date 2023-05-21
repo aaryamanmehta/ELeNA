47f83836a6bcc8e6cff1949f8da0fa649e1c5ded
Aryan <aryandang43@gmail.com>
2023-05-20 21:52:28 -0400

    adds code to get the shortest path

--- src/backend/controller/shortestPathNoElevation.py
+++ src/backend/controller/shortestPathNoElevation.py
@@ -1,2 +1,11 @@
 from DataMod.GraphHelper import *
+from DataMod.dijkstra import *
 
+def get_shortest_path(source_request : str, destination_request : str):
+    source_info = get_location(source_request)
+    destination_info = get_location(destination_request)
+    graph = generate_graph(source_info, destination_info)
+    source_coords = get_coordinates(source_request)
+    destination_coords = get_coordinates(destination_request)
+    path = dijkstra(source_coords, destination_coords, graph)
+    return path
